b442f4f9085b9f7c5ba414030884792ea7b17add
Kaio Abreu <kaio.freires.abreu@gmail.com>
2025-10-05 16:32:35 -0300

    Primeiro merging para a branch main concluido com sucesso no dia 05/10/2025

---
+++ app/src/main/java/com/jobmatch/TelaMudancaSucesso.kt
@@ -0,0 +1,22 @@
+package com.jobmatch
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TelaMudancaSucesso : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tela_mudanca_sucesso)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // FUNÇÃO PARA VOLTAR PARA A TELA INICIAL (REUTILIZAR EM OUTRAS TELAS DEPOIS)
+    }
+}
